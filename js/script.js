fc266e21211fcd71451b2267b90353742b8add34
Andrey Solzig <solzig@mail.ru>
2019-04-01 17:20:07 +0300

    Сверстан блок категорий, без карусели

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){
+    $('.hero-slider').owlCarousel({
+        nav:false,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            }, 
+            600: {
+                items: 1
+            }, 
+            900: {
+                items: 1
+            }, 
+            1200: {
+                items: 1
+            }
+        }
+    })
+  });
+
 var menuButton = document.getElementById('menuButton');
 var menu = document.getElementById('menuList');
 
